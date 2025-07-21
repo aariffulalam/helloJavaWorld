7a602d5dd2d480612a85660db7059a59f5d4ab10
Aarif Alam <aariffulalam36@gmail.com>
2025-07-21 23:14:04 +0530

    data types

---
+++ Topic1BasicOfJava/Lesson1IntroductionToJava/Lecture2DataTypes.java
@@ -0,0 +1,34 @@
+package Topic1BasicOfJava.Lesson1IntroductionToJava;
+
+public class Lecture2DataTypes {
+    /*
+     * Java is a statically and highly typed language since each type of data is predefined as part of the programming language.
+     * A variable's data type is an attribute that tells the compiler how the programmer wants to utilize the variable. It specifies the actions that may perform on the data and the types of values that can be stored.
+     * 
+     * Java has two types of data: 
+        * Primitive Data Type
+        * Non-Primitive Data Type or Object Data Type
+        
+     * Primitive Data Types:
+     * The programming language pre-defines a primitive data type. It contains no extra sophisticated methods and specifies the size and type of variable values.
+     * There are eight kinds of primitive data types supported by Java:
+        * Boolean data type
+            * A boolean data type comprises a single bit of data that can only contain true or false values.
+        * byte data type
+            * It's an 8-bit two-s complement signed integer. It stores numbers in the range of -128 to 127. A byte data type helps conserve significant quantities of memory.
+        * char data type
+            * A char data type is a single 16-bit Unicode character. It can store any character, including letters, digits, and symbols.
+        * short data type
+            * The short data type is a 16-bit signed two's complement integer. It keeps track of values ranging from -32,768 to 32767. Use a short to conserve memory in big arrays, similar to byte.
+        * int data type
+            * It is a 4 Bytes or 32-bit signed two's complement integer. Whole numbers between -2147483648 and 2147483647 can be stored in this data format. It is the most commonly used data type for integers.
+        * long data type
+            * It is an 8 Bytes, or 64-bit signed two's complement integer. It can store values ranging from -9223372036854775808 to 9223372036854775807. It is used when a wider range than int is required.
+        * float data type
+            * A float data type is a single-precision 32-bit IEEE 754 floating point. It is used to store decimal numbers with a precision of about 7 decimal digits.
+        * double data type
+            * A double data type is a double-precision 64-bit IEEE 754 floating point. It is used to store decimal numbers with a precision of about 15 decimal digits. It is the default data type for decimal values in Java.
+     
+
+    */
+}
