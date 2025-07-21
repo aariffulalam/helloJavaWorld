fb62e16f7f7a29a853a8915141a5cd4489180f6a
Aarif Alam <aariffulalam36@gmail.com>
2025-07-21 22:23:17 +0530

    How to run Java program

---
+++ Topic1BasicOfJava/Lesson1IntroductionToJava/Lecture1HowToRunJava.java
@@ -0,0 +1,33 @@
+package Topic1BasicOfJava.Lesson1IntroductionToJava;
+
+public class Lecture1HowToRunJava {
+    /*
+     * Java is an object-oriented language.It enables automatic memory management. It also has multi-threading. It has a vast standard library. It is open-source so you can use it for free. You can use it to make various web and mobile applications. 
+     * 
+     * You write a Java program in the Java programming language. You can run a Java program on a Java Virtual Machine (JVM). JVM converts Java bytecode to machine code. Your Java programs are portable.
+    */
+
+    /*
+     * step 1 - Installing Java Development Kit (JDK)
+        You need to have JDK to run a Java program. So you can download the JDK from the Oracle's official site. (https://www.oracle.com/in/java/technologies/downloads/)
+
+        * step 2 - Open code editor
+        Write your Java program. Now save it with a .java extension. Here it is HelloWorld.java.
+
+        public class HelloWorld {
+            public static void main(String[] args) {
+                System.out.println("Hello, World!");
+            }
+        }
+
+        * step 3 - Compile the Java program
+        Open the command prompt or terminal. Navigate to the directory where you saved your Java file. Use the following command to compile your Java program:
+        javac HelloWorld.java
+        This command will create a file named HelloWorld.class in the same directory. This file contains the bytecode of your Java program.
+        
+        * step 4 - Run the Java program
+        Now you can run your Java program using the following command:
+        java HelloWorld
+        This command will execute the bytecode in the HelloWorld.class file and print "Hello, World!" to the console.
+    */
+}
