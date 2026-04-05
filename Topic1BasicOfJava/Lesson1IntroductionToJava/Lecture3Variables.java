88b0dc989ce4f5225854a748c7170b684cccfa54
Aarif Alam <aariffulalam36@gmail.com>
2026-04-05 16:04:54 +0530

    lacture 3 variables

---
+++ Topic1BasicOfJava/Lesson1IntroductionToJava/Lecture3Variables.java
@@ -0,0 +1,27 @@
+package Topic1BasicOfJava.Lesson1IntroductionToJava;
+
+public class Lecture3Variables {
+    /*
+     * Programs are used to perform specific tasks, which may include storing, manipulating or printing data. The involvement of data in a program introduces the need for variables. A variable is a container that temporarily saves a data value during the execution of a program. They label and store data in the memory for easy access. The amount of space allocated to a variable in the memory depends on the type of value it holds.
+     * 
+     * Variables in Java
+        * Variables are also called identifiers in Java. Every programming language has its set of rules and conventions for naming a variable, and Java is no different.
+        * A variable name can contain alphanumeric characters, the underscore and dollar characters.
+        * A variable name must always start with a letter as ‘_’ and ‘$’ are not conventionally used.
+        * Whitespaces are not permitted.
+        * Keywords can not be used as variable names.
+        * Variable names are case-sensitive.
+     *
+     * Declaration
+        * Declaration of a variable is the process of creating and introducing the variable into your program. It lets the compiler know about the identifier’s name and the type of data it will hold. Once declared, a variable can not change its type throughout the program.
+        * Syntax: 
+            dataType variableName;
+        * Installation
+            data_type variable_name = value;
+     *
+     * Types of variables in Java:
+        * Local variables
+            * Local variables are declared within a method, constructor, or block. They are only accessible within that method, constructor, or block and are not visible outside of it. Local variables must be initialized before use.
+            * Sytax: 
+    */
+}
